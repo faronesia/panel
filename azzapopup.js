1a8f56f0ab1ba371f295bea75cb44c4f8d7b00ca
Hazira Azza Faroni <60853582+azzaproject@users.noreply.github.com>
2021-04-20 21:44:01 +0700

    Add files via upload

---
+++ azzapopup.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){$('.close-sf')
+.click(function(){$('.mega-popup,.mega-popup-off,')
+.removeClass('aktif')})}),$(document)
+.ready(function(){$('#openpop')
+.click(function(){$('.mega-popup,.mega-popup-off')
+.toggleClass('aktif')})});
